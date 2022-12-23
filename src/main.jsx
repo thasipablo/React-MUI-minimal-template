bdbcb8a813f69cf8ddc0a3d12136811ea8c7a287
thasipablo <thasipablo@gmail.com>
2022-12-23 12:28:23 +0100

    add custom theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import ThemeProvider from "./theme";
 import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 
 const Home = () => {
   return (
-    <Box>
-      <Typography variant="h1">Home</Typography>
+    <Box p={4}>
+      <Typography variant="h1">Home h1</Typography>
+      <Typography variant="h2">Home h2</Typography>
+      <Typography variant="h3">Home h3</Typography>
+      <Typography variant="h4">Home h4</Typography>
+      <Typography variant="h5">Home h5</Typography>
+      <Typography variant="h6">Home h6</Typography>
+      <Button variant="contained">btn</Button>
     </Box>
   );
 };
